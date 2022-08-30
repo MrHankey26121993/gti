5c70fd4c11a1992ed63d3b0a415c3b439bcfe794
Ruslan <mrhankey26>
2022-08-30 11:12:11 +0300

    +

--- static/js/index.js
+++ static/js/index.js
@@ -1,5 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
 
+    let bodyWidth = document.querySelector('body').clientWidth;
+
     const observer = new IntersectionObserver(entries => {
         // перебор записей
         entries.forEach(entry => {
@@ -10,14 +12,30 @@ document.addEventListener('DOMContentLoaded', () => {
                     entry.target.classList.remove('opacity-0');
                     entry.target.classList.add('fadeInDownBig');
                 }
-                if (entry.target.classList.contains('animated-left')) {
-                    entry.target.classList.remove('opacity-0');
-                    entry.target.classList.add('fadeInLeft');
+                if (bodyWidth > 600) {
+                    if (entry.target.classList.contains('animated-left')) {
+                        entry.target.classList.remove('opacity-0');
+                        entry.target.classList.add('fadeInLeft');
+                    }
+                    if (entry.target.classList.contains('animated-right')) {
+                        entry.target.classList.add('fadeInRight');
+                        entry.target.classList.remove('opacity-0');
+                    }
+                    if (entry.target.classList.contains('team-animated-teamated')) {
+                        entry.target.classList.add('fadeInLeft');
+                        entry.target.classList.remove('opacity-0');
+                    }
+                } else {
+                    if (entry.target.classList.contains('animated-right-mob')) {
+                        entry.target.classList.add('fadeInRight');
+                        entry.target.classList.remove('opacity-0');
+                    }
+                    if (entry.target.classList.contains('animated-left-mob')) {
+                        entry.target.classList.remove('opacity-0');
+                        entry.target.classList.add('fadeInLeft');
+                    }
                 }
-                if (entry.target.classList.contains('animated-right')) {
-                    entry.target.classList.add('fadeInRight');
-                    entry.target.classList.remove('opacity-0');
-                }
+
                 if (entry.target.classList.contains('animated-title')) {
                     entry.target.classList.add('fadeInDown');
                     entry.target.classList.remove('opacity-0');
@@ -26,25 +44,37 @@ document.addEventListener('DOMContentLoaded', () => {
                     entry.target.classList.add('fadeInLeft');
                     entry.target.classList.remove('opacity-0');
                 }
-                if (entry.target.classList.contains('team-animated-teamated')) {
-                    entry.target.classList.add('fadeInLeft');
-                    entry.target.classList.remove('opacity-0');
-                }
+
 
 
             } else {
                 if (entry.target.classList.contains('main-animated')) {
                     entry.target.classList.add('opacity-0');
-                    //      entry.target.classList.remove('fadeInDownBig');
                 }
-                if (entry.target.classList.contains('animated-left')) {
-                    entry.target.classList.add('opacity-0');
-                    entry.target.classList.remove('fadeInLeft');
+                if (bodyWidth > 600) {
+                    if (entry.target.classList.contains('animated-left')) {
+                        entry.target.classList.add('opacity-0');
+                        entry.target.classList.remove('fadeInLeft');
+                    }
+                    if (entry.target.classList.contains('animated-right')) {
+                        entry.target.classList.add('opacity-0');
+                        entry.target.classList.remove('fadeInRight');
+                    }
+                    if (entry.target.classList.contains('team-animated-teamated')) {
+                        entry.target.classList.remove('fadeInLeft');
+                        entry.target.classList.add('opacity-0');
+                    }
+                } else {
+                    if (entry.target.classList.contains('animated-right-mob')) {
+                        entry.target.classList.add('opacity-0');
+                        entry.target.classList.remove('fadeInRight');
+                    }
+                    if (entry.target.classList.contains('animated-left-mob')) {
+                        entry.target.classList.add('opacity-0');
+                        entry.target.classList.remove('fadeInLeft');
+                    }
                 }
-                if (entry.target.classList.contains('animated-right')) {
-                    entry.target.classList.add('opacity-0');
-                    entry.target.classList.remove('fadeInRight');
-                }
+
                 if (entry.target.classList.contains('animated-title')) {
                     entry.target.classList.remove('fadeInDown');
                     entry.target.classList.add('opacity-0');
@@ -53,10 +83,7 @@ document.addEventListener('DOMContentLoaded', () => {
                     entry.target.classList.remove('fadeInLeft');
                     entry.target.classList.add('opacity-0');
                 }
-                if (entry.target.classList.contains('team-animated-teamated')) {
-                    entry.target.classList.remove('fadeInLeft');
-                    entry.target.classList.add('opacity-0');
-                }
+
 
 
             }
@@ -67,19 +94,25 @@ document.addEventListener('DOMContentLoaded', () => {
     })
 
     let blockTeamated = document.querySelectorAll('.teamated');
+    let widthMain = null,
+        heightMain = null;
     blockTeamated.forEach(item => {
-
-        item.style.height = item.clientWidth + 'px';
+        if (!widthMain) {
+            widthMain = item.clientWidth;
+            heightMain = widthMain;
+        }
+        item.style.width = widthMain + 'px';
+        item.style.height = widthMain + 'px';
         let ava = item.querySelector('.teamated-ava');
         ava.style.position = 'absolute';
-        let avaWidth = item.clientWidth - (item.clientWidth / 100 * 50)
+        let avaWidth = widthMain - (widthMain / 100 * 50)
         ava.style.width = avaWidth + 'px'; //Ширина 50% от общей ширины элемента
         ava.style.height = ava.style.width;
         ava.style.borderRadius = '50%';
         ava.style.backgroundImage = `url(../../static/img/${item.id}.jpg)`;
         ava.style.backgroundSize = 'cover';
         ava.style.backgroundPosition = 'center';
-        ava.style.left = (item.clientWidth - avaWidth) / 2 + 'px';
+        ava.style.left = (widthMain - avaWidth) / 2 + 'px';
         ava.style.top = '-' + (avaWidth / 2) + 'px';
         item.style.marginTop = (avaWidth / 2) + 'px';
         let info = item.querySelector('.teamated-info');
